fea9486e667ea35381dc0da2da1deef8da338fbb
Sabbay <Sabbay@bitbucket.org>
2017-01-25 21:08:32 +0100

    Wrapped this in object

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -4,144 +4,172 @@
 #include <assert.h>
 
 
-void print(Node* head) {
+
+
+List new(Node *head, Node *tail){
+  List *l = calloc(1, sizeof(List));
+  l->head = head;
+  l->tail = tail;
+  if(l->tail != 0x0){
+    l->tail->next = 0x0; // we want to cut off the list if it was created from only a part of a bigger list
+  }
+  return *l;
+}
+
+void print(List *l) {
   int index=0;
-  if(head == 0x0){
+  if(l->head == 0x0){
     return;
   }
-  while(head != 0x0){
-    printf("%d:[%d, %x] ", index, head->value, head);
+
+  Node *cur = l->head;
+
+  while(cur != 0x0){
+    printf("%d:[%d, %x] ", index, cur->value, cur);
     index++;
-    head = head->next;
+    cur = cur->next;
   }
+  printf("\n");
+  printf("size %d   head %x   tail %x", l->size, l->head, l->tail);
   printf("\n");
 }
 
 
-bool empty(Node* head) {
-  if(head == 0x0){
+bool empty(List *l) {
+  if(l->head == 0x0){
     return TRUE;
   }
   return FALSE;
 }
 
-uint16_t size(Node* head) {
+uint16_t size(List *l) {
   uint16_t n=0;
-  if(head == 0x0){
+  if(l->head == 0x0){
     return 0; // empty list
   }
-  while(head != 0x0){
-    head = head->next;
+
+  Node *cur = l->head;
+
+  while(cur!= 0x0){
+    cur= cur->next;
     n++;
   }
   return n;
 }
 
 
-int value_at(Node* head, int index) {
+int value_at(List *l, int index) {
   int c_indx = 0;
+  Node *cur = l->head;
   while(c_indx < index){
     c_indx++;
-    if(head == 0x0){
+    if(l->head == 0x0){
       printf("invalid index\n"); 
       exit(-1);
     }
-    head = head->next;
+    cur= cur->next;
   }
-  return head->value;
+  return cur->value;
 }
 
 
-void push_front(Node** head, int value) {
+void push_front(List *l, int value) {
   Node *new_node =calloc(1, sizeof(Node));
   new_node->value = value;
-  new_node->next = *head;
-  *head = new_node;
+  new_node->next = l->head;
+  if(l->head == 0x0){
+    l->tail = new_node; // first elemnt of a one element list is at the same time tail and head
+  }
+  l->head = new_node;
+  l->size++;
 }
 
 
-int pop_front(Node** head) {
+int pop_front(List *l) {
   Node *tmp;
   int value;
-  if(*head == 0x0){
+  if(l->head == 0x0){
     printf("cannot pop empty list...");
     exit(-1);
   }
-  tmp = *head;
+  tmp = l->head;
   value = tmp->value;
-  *head = tmp->next;
+  l->head = tmp->next;
   free(tmp);
+  l->size--;
   return value;
 }
 
 
-void push_back(Node** head, int value) {
+void push_back(List *l, int value) {
   Node *new_node = calloc(1, sizeof(Node)); // next is already 0x0 thanks to calloc
   new_node->value = value;
 
-  if(*head == 0x0){
-    *head = new_node;
+  if(l->head == 0x0){
+    l->head = new_node;
+    l->tail = new_node;
+    l->size++;
     return;
   }
 
-  int n =0;
-  Node *cur = *head;
-  while( cur->next != 0x0){
-    n++;
-    cur = cur->next;
-  }
-  cur->next = new_node;
+  l->tail->next = new_node;
+  l->tail = new_node;
+  l->size++;
 
 }
 
-int pop_back(Node **head){
+int pop_back(List *l){
   int value;
   Node *previous = 0x0;
-  if(head == 0x0){
+  if(l->head == 0x0){
     printf("Cannot pop_back empty list");
     exit(-1);
   }
-  Node *cur = *head;
+  Node *cur = l->head;
   while(cur ->next != 0x0){
     previous = cur;
     cur = cur->next;
   }
   if(previous != 0x0){
     previous->next = 0x0;
+    l->tail = previous;
   }else{
-    *head = 0x0;
+    l->head = 0x0;
+    l->tail = 0x0;
   }
   value = cur->value;
   free(cur);
+  l->size--;
   return value;
 }
 
-int front(Node *head){
-  if(head == 0x0){
+int front(List *l){
+  if(l->head == 0x0){
     printf("empty list");
     exit(-1);
   }
-  return head->value;
+  return l->head->value;
 }
 
-int back(Node *head){
-  if(head == 0x0){
+int back(List *l){
+  if(l->head == 0x0){
     printf("empty list");
     exit(-1);
   }
-  while(head->next != 0x0){
-    head = head->next;
+  Node *cur = l->head;
+  while(cur->next != 0x0){
+    cur = cur->next;
   }
-  return head->value;
+  return cur->value;
 }
 
-int value_n_from_end(Node *head, int n){
-  if(head == 0x0 || n<1){
+int value_n_from_end(List *l, int n){
+  if(l->head == 0x0 || n<1){
     printf("invalid n");
     exit(-1);
   }
-  Node *left_bound = head;
-  Node *right_bound = head;
+  Node *left_bound = l->head;
+  Node *right_bound = l->head;
   int i;
   for(i=0; i<n && right_bound!=0x0; i++){
     right_bound = right_bound->next;
@@ -158,12 +186,12 @@ int value_n_from_end(Node *head, int n){
 }
 
 
-void erase(Node **head, int index){
+void erase(List *l, int index){
   int i;
   Node *prev = 0x0;
-  Node *cur = *head;
+  Node *cur = l->head;
 
-  if(*head == 0x0){
+  if(l->head == 0x0){
     printf("Unable to erase from empty list");
   }
 
@@ -181,17 +209,18 @@ void erase(Node **head, int index){
   if(prev != 0x0){
     prev->next = cur->next;
   }else{
-    *head =  cur->next;
+    l->head =  cur->next;
   }
+  l->size--;
   free(cur);
 
 }
 
-void insert(Node **head, int index, int value){
+void insert(List *l, int index, int value){
 
   int i;
   Node *prev = 0x0;
-  Node *cur = *head;
+  Node *cur = l->head;
 
   Node *new_node = calloc(1, sizeof(Node));
   new_node->value = value;
@@ -206,25 +235,26 @@ void insert(Node **head, int index, int value){
     return;
   }
 
+  if(cur == 0x0){l->tail = new_node; }
   if(prev == 0x0){
-    new_node->next = *head;
-    *head = new_node;
+    new_node->next = l->head;
+    l->head = new_node;
   }else{
     prev->next = new_node;
     new_node->next = cur;
   }
-
+  l->size++;
 }
 
 
-void reverse(Node **head){
-  if(*head == 0x0){
+void reverse(List *l){
+  if(l->head == 0x0){
     printf("Cannod reverse empty list");
     exit(-1);
   }
   Node *left, *mid, *right;
-  mid = *head;
-  right = *head;
+  mid = l->head;
+  right = l->head;
   left = 0x0;
 
   while(mid != 0x0){
@@ -234,28 +264,29 @@ void reverse(Node **head){
     mid = right;
   }
 
-  *head = left;
+  l->head = left;
 
 }
 
 
 
-void remove_value(Node **head, int value){
+void remove_value(List *l, int value){
   Node *prev = 0x0;
-  Node *cur = *head;
+  Node *cur = l->head;
   bool found = FALSE;
-  if(*head == 0x0){
+  if(l->head == 0x0){
     return;
   }
 
   while(cur != 0x0){
     if(cur->value == value){
       if(prev == 0x0){
-        *head = cur->next;
+        l->head = cur->next;
       }else{
         prev->next = cur->next;
       }
       found = TRUE;
+      l->size--;
       free(cur);
     }
     if(found){
@@ -292,161 +323,195 @@ void test_all(){
 
 
 void test_remove_value(){
-  Node *head = 0x0;
-  push_back(&head, 2);
-  push_back(&head, 2);
-  remove_value(&head, 2);
-  print(head);
-  assert(empty(head) == TRUE);
-  push_back(&head, 2);
-  push_back(&head, 2);
-  push_back(&head, 3);
-  remove_value(&head, 2);
-  assert(value_at(head, 0) == 3);
-  push_back(&head, 2);
-  push_back(&head, 3);
-  remove_value(&head, 3);
-  assert(value_at(head, 0) == 2 );
+  List l = new(0x0, 0x0);
+  push_back(&l, 2);
+  push_back(&l, 2);
+  
+  assert(l.size == size(&l));
+  remove_value(&l, 2);
+  assert(l.size == size(&l));
+  
+  assert(empty(&l) == TRUE);
+  push_back(&l, 2);
+  push_back(&l, 2);
+  push_back(&l, 3);
+  remove_value(&l, 2);
+  assert(value_at(&l, 0) == 3);
+  push_back(&l, 2);
+  push_back(&l, 3);
+  remove_value(&l, 3);
+  assert(value_at(&l, 0) == 2 );
 }
 
 void test_reverse(){
-  Node *head = 0x0;
-  push_back(&head, 1);
-  reverse(&head);
-  assert(value_at(head, 0) == 1);
-  push_back(&head, 2);
-  push_back(&head, 3);
-  reverse(&head);
-  assert(value_at(head, 0) == 3);
-  assert(value_at(head, 1) == 2);
-  assert(value_at(head, 2) == 1);
+  List l = new(0x0, 0x0);
+  push_back(&l, 1);
+  reverse(&l);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 0) == 1);
+  push_back(&l, 2);
+  push_back(&l, 3);
+  reverse(&l);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 0) == 3);
+  assert(value_at(&l, 1) == 2);
+  assert(value_at(&l, 2) == 1);
 }
 
 void test_erase(){
-  Node *head = 0x0;
-  push_back(&head, 1);
-  erase(&head, 0);
-  assert(empty(head) == TRUE);
-  push_back(&head, 1);
-  push_back(&head, 2);
-  erase(&head, 0);
-  assert(size(head) == 1);
-  assert(value_at(head, 0) == 2);
-  erase(&head, 0);
-  assert(empty(head) == TRUE);
+  List l = new(0x0, 0x0);
+  push_back(&l, 1);
+  erase(&l, 0);
+  assert(l.size == size(&l));
+  assert(empty(&l) == TRUE);
+  push_back(&l, 1);
+  push_back(&l, 2);
+  erase(&l, 0);
+  assert(l.size == size(&l));
+  assert(size(&l) == 1);
+  assert(value_at(&l, 0) == 2);
+  erase(&l, 0);
+  assert(empty(&l) == TRUE);
 
+  assert(l.size == size(&l));
 
-  push_back(&head, 2);
-  push_back(&head, 3);
-  push_back(&head, 4);
-  push_back(&head, 5);
-  erase(&head, 3);
-  assert(size(head) == 3);
+  push_back(&l, 2);
+  push_back(&l, 3);
+  push_back(&l, 4);
+  push_back(&l, 5);
+  erase(&l, 3);
+  assert(size(&l) == 3);
 }
 
 void test_insert(){
-  Node *head = 0x0;
-  insert(&head, 0, 10);
-  assert(value_at(head, 0) == 10);
-  insert(&head, 0, 11);
-  insert(&head, 0, 12);
-  insert(&head, 2, 22);
-  insert(&head, 4, 44);
-  assert(value_at(head, 0) == 12 );
-  assert(value_at(head, 1) == 11 );
-  assert(value_at(head, 2) == 22 );
-  assert(value_at(head, 3) == 10 );
-  assert(value_at(head, 4) == 44 );
+  List l = new(0x0, 0x0);
+  insert(&l, 0, 10);
+  
+  assert(value_at(&l, 0) == 10);
+  insert(&l, 0, 11);
+  insert(&l, 0, 12);
+  insert(&l, 2, 22);
+  insert(&l, 4, 44);
+  
+  assert(l.size == size(&l));
+  assert(value_at(&l, 0) == 12 );
+  assert(value_at(&l, 1) == 11 );
+  assert(value_at(&l, 2) == 22 );
+  assert(value_at(&l, 3) == 10 );
+  assert(value_at(&l, 4) == 44 );
 }
 
 void test_back(){
-  Node *head = 0x0;
-  push_back(&head, 5);
-  push_back(&head, 4);
-  assert(back(head) == 4);
+  List l = new(0x0, 0x0);
+  push_back(&l, 5);
+  push_back(&l, 4);
+  assert(l.size == size(&l));
+  assert(back(&l) == 4);
 }
 
 void test_front(){
-  Node *head = 0x0;
-  push_front(&head, 5);
-  push_front(&head, 4);
-  assert(front(head) == 4);
+  List l = new(0x0, 0x0);
+  push_front(&l, 5);
+  push_front(&l, 4);
+  assert(l.size == size(&l));
+  assert(front(&l) == 4);
 }
 
 void test_pop_back(){
-  Node *head = 0x0;
-  push_back(&head, 5);
-  push_back(&head, 3);
-  push_back(&head, 2);
-  assert(size(head) == 3);
-  assert(pop_back(&head) == 2);
-  assert(pop_back(&head) == 3);
-  assert(pop_back(&head) == 5);
-  assert(size(head)== 0);
+  List l = new(0x0, 0x0);
+  push_back(&l, 5);
+  push_back(&l, 3);
+  push_back(&l, 2);
+  assert(l.size == size(&l));
+  assert(size(&l) == 3);
+  assert(pop_back(&l) == 2);
+  
+  assert(pop_back(&l) == 3);
+  assert(l.size == size(&l));
+  assert(pop_back(&l) == 5);
+  assert(l.size == size(&l));
+  
+  assert(l.tail == 0x0);
+  assert(size(&l)== 0);
 }
 
 void test_push_back(){
-  Node *head = 0x0;
-  push_back(&head, 10);
-  push_back(&head, 11);
-  push_back(&head, 12);
-  push_back(&head, 13);
-  assert(size(head) == 4);
-  assert(value_at(head, 0) == 10);
-  assert(value_at(head, 1) == 11);
-  assert(value_at(head, 2) == 12);
-  assert(value_at(head, 3) == 13);
+  List l = new(0x0, 0x0);
+  push_back(&l, 10);
+  push_back(&l, 11);
+  push_back(&l, 12);
+  push_back(&l, 13);
+  assert(l.size == size(&l));
+  assert(size(&l) == 4);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 0) == 10);
+  assert(value_at(&l, 1) == 11);
+  assert(value_at(&l, 2) == 12);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 3) == 13);
 }
 
 void test_pop_front(){
-  Node *head = 0x0;
-  push_front(&head, 10);
-  assert(pop_front(&head) == 10);
-  assert(empty(head));
+  List l = new(0x0, 0x0);
+  push_front(&l, 10);
+  assert(l.size == size(&l));
+  
+  assert(pop_front(&l) == 10);
+  assert(l.size == size(&l));
+  assert(empty(&l));
 }
 
 void test_value_at(){
-  Node *head = 0x0;
-  push_front(&head, 5);
-  assert(value_at(head, 0) == 5);
-  push_front(&head, 6);
-  assert(value_at(head, 1) == 5);
-  assert(value_at(head, 0) == 6);
+  List l = new(0x0, 0x0);
+  push_front(&l, 5);
+  assert(value_at(&l, 0) == 5);
+  push_front(&l, 6);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 1) == 5);
+  assert(l.size == size(&l));
+  assert(value_at(&l, 0) == 6);
 }
 
 void test_push_front(){
-   Node *head = 0x0;
-   push_front(&head, 1);
-   push_front(&head, 2);
-   push_front(&head, 3);
-   push_front(&head, 4);
-   push_front(&head, 5);
-   assert(size(head) == 5);
-   assert(value_at(head, 0) == 5);
-   assert(value_at(head, 1) == 4);
-   assert(value_at(head, 2) == 3);
-   assert(value_at(head, 3) == 2);
-   assert(value_at(head, 4) == 1);
+   List l = new(0x0, 0x0);
+   push_front(&l, 1);
+   push_front(&l, 2);
+   push_front(&l, 3);
+   assert(l.size == size(&l));
+   push_front(&l, 4);
+   push_front(&l, 5);
+   
+   assert(size(&l) == 5);
+   assert(value_at(&l, 0) == 5);
+   assert(value_at(&l, 1) == 4);
+   assert(l.size == size(&l));
+   assert(value_at(&l, 2) == 3);
+   assert(value_at(&l, 3) == 2);
+   assert(value_at(&l, 4) == 1);
 }
 
 void test_empty(){
-  Node *head = 0x0;
-  assert( empty(head) == TRUE);
-  push_front(&head, 5);
-  assert( empty(head) == FALSE);
+  List l = new(0x0, 0x0);
+  assert( empty(&l) == TRUE);
+  assert(l.size == size(&l));
+  push_front(&l, 5);
+  assert( empty(&l) == FALSE);
+  assert(l.size == size(&l));
 }
 
 
 void test_value_n_from_end() {
-  Node *head = 0x0;
-  push_back(&head, 1);
-  push_back(&head, 2);
-  push_back(&head, 3);
-  push_back(&head, 4);
-  push_back(&head, 5);
-  assert(value_n_from_end(head, 1) == 5);
-  assert(value_n_from_end(head, 3) == 3);
-  assert(value_n_from_end(head, 5) == 1);
+  List l = new(0x0, 0x0);
+  push_back(&l, 1);
+  push_back(&l, 2);
+  assert(l.size == size(&l));
+  push_back(&l, 3);
+  push_back(&l, 4);
+  push_back(&l, 5);
+  assert(value_n_from_end(&l, 1) == 5);
+  assert(value_n_from_end(&l, 3) == 3);
+  assert(value_n_from_end(&l, 5) == 1);
+  assert(l.size == size(&l));
 }
 
 
